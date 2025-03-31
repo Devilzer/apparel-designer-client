edf397b1c50ea5c711b2a49d5d765563c07a424e
devilzer <jenadeepak724@gmail.com>
2025-04-01 00:04:39 +0530

    WIP: Implement main application structure with Canvas, Customizer, and Home components; add configuration and helper functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-function App() {
+import Canvas from './canvas/index'
+import Customizer from './features/customizer/container'
+import Home from './features/home/container'
 
+function App() {
   return (
-    <div className="app">
+    <div className="app transition-all ease-in">
+      <Home />
+      <Canvas />  
+      <Customizer />
     </div>
   )
 }
---
+++ src/features/home/container/index.tsx
@@ -0,0 +1,12 @@
+import {motion, AnimatePresence} from 'framer-motion';
+import {useSnapshot} from 'valtio'
+
+import {headContentAnimation, headContainerAnimation, headTextAnimation} from '../../../config/motion.ts'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
